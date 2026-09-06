4fbf37cdbb08dd58226069848f5bf51bfb8bff48
turtNova <gudturtleboi@gmail.com>
2026-09-06 21:38:46 +0200

    added lsp binds

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,3 +1,11 @@
 -- Below text inline hints
 -- vim.diagnostic.config({ virtual_lines = true })
 vim.diagnostic.config({ virtual_text = true })
+
+vim.keymap.set("n", "<leader>gd", function()
+    vim.diagnostic.open_float(0, { scope = "line" })
+end)
+vim.keymap.set("n", "gd", function()
+    vim.lsp.buf.definition()
+    -- { loclist = true }
+end)
